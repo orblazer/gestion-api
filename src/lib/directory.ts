bd0f08bde72ca0df576e3530b51db84b6bec6bd5
orblazer <orblazer.minecraft@gmail.com>
2019-05-28 18:33:53 +0200

    feat(lib): add walk directory

--- src/lib/directory.ts
+++ src/lib/directory.ts
@@ -1,4 +1,5 @@
 import Path from 'path'
+import fs from 'fs-extra'
 
 export function normalize (path: string): string {
   return (
@@ -8,4 +9,21 @@ export function normalize (path: string): string {
       .split(/[\\\/]/g)
       .join('/')
   )
+}
+
+export async function walk (dir: string): Promise<string[]> {
+  const stat = await fs.stat(dir)
+  if (!stat.isDirectory()) {
+    throw new Error(`The path "${dir}" is not directory`)
+  }
+
+  const files = await Promise.all(
+    (await fs.readdir(dir)).map(
+      async (file): Promise<string[] | string> => {
+        file = Path.join(dir, file)
+        return (await fs.stat(file)).isDirectory() ? walk(file) : file
+      }
+    )
+  )
+  return Array.prototype.concat(...files)
 }
