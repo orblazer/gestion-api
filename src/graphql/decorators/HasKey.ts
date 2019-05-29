7ce0124b0ddad8698e645e65cf612e303660b019
orblazer <orblazer.minecraft@gmail.com>
2019-05-29 12:42:53 +0200

    refactor(graphql): simplify import has key

--- src/graphql/decorators/HasKey.ts
+++ src/graphql/decorators/HasKey.ts
@@ -1,6 +1,11 @@
 import { UseMiddleware, ForbiddenError } from 'type-graphql'
 import { MethodAndPropDecorator } from 'type-graphql/dist/decorators/types'
 
+/**
+ * Check if key id graphql editor key
+ *
+ * @param key the key
+ */
 export function isEditorKey (key: string): boolean {
   if (
     key === 'admin:yc6p39p7BYFx7QAiYqzJ29Ku2Hr46fS2' &&
@@ -10,7 +15,7 @@ export function isEditorKey (key: string): boolean {
   }
 }
 
-export function HasKey (keyFn?: () => string): MethodAndPropDecorator {
+export default function HasKey (keyFn?: () => string): MethodAndPropDecorator {
   return UseMiddleware(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ({ context: { key } }, next): Promise<any> => {
@@ -19,11 +24,12 @@ export function HasKey (keyFn?: () => string): MethodAndPropDecorator {
         return next()
       }
 
+      // Check if key match to needed key
       const tryKey = typeof keyFn === 'function' ? keyFn() : true
-
       if ((tryKey === true && key === null) || key !== tryKey) {
         throw new ForbiddenError()
       }
+
       return next()
     }
   )
