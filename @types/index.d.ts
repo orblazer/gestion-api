628894dcd9d0428d005d1f27ed25e562db1e0c96
orblazer <orblazer.minecraft@gmail.com>
2019-05-29 13:03:51 +0200

    refactor(types): remove useless methods

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,4 +1,3 @@
-import { Website as WebsiteDB } from 'src/database/Website'
 import { UserJWT } from 'src/database/User'
 
 declare interface ReplaceFunction {
@@ -6,30 +5,8 @@ declare interface ReplaceFunction {
   (substring: string, ...args: any[]): string;
 }
 
-declare namespace Database {
-  interface UserPassword {
-    salt: string;
-    hash: string;
-    iterations: number;
-  }
-
-  interface TextLocalized {
-    [key: string]: string;
-  }
-}
-
-declare namespace Website {
-  type InitializeFunction = (context: InitializeContext, website: WebsiteDB, apiKey: string) => Promise<void> | void
-
-  interface InitializeContext {
-    readonly apiURL: string;
-    markdown (text: string, baseUrl?: string): string;
-    replaceName (name: string, limiter?: string): RegExp;
-    replaceInFile (
-      path: string,
-      replaces?: { search: string | RegExp; value: string | ReplaceFunction }[]
-    ): Promise<string>;
-  }
+declare interface TextLocalized {
+  [key: string]: string;
 }
 
 declare interface GraphqlContext {
